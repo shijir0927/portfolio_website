c83e9212ed664e9c6789269b7e3fb587e4d0d787
Shijir <shijiree_mng@yahoo.com>
2021-05-06 21:49:12 +0800

    footer update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,53 @@
 import React from "react";
 import "./style.css";
-
+import * as Scroll from 'react-scroll';
+import {Icon} from 'react-fa'
 const Footer = () => {
+
+  const scrollTo = (className) => {
+    // smooth scolling
+    const scroller = Scroll.scroller;
+    scroller.scrollTo( className, {
+        duration: 1000,
+        delay: 0,
+        smooth: 'easeInOutQuart'
+    })
+  }
+
   return (
     <div className="my-footer">
-      <p>Shijir-Erdene Bat-Enkh © 2021</p>
+      <div className="footer-up" onClick={()=>scrollTo('intro')}>
+        <Icon name="angle-double-up"/>
+      </div>
+      <div className="footer-icons">
+        <a className="footer-icon-container"
+        href="https://github.com/shijir0927"
+        target="_blank"
+        rel="noopener noreferrer">
+          <Icon name="github"/>
+        </a>
+        <a className="footer-icon-container"
+            href="https://www.linkedin.com/in/shijirerdenebatenkh/"
+            target="_blank"
+            rel="noopener noreferrer">
+          <Icon name="linkedin"/>
+        </a>
+        <a className="footer-icon-container"
+        href="mailto:sbatenkh@ucdavis.edu"
+        target="_blank"
+        rel="noopener noreferrer">
+          <Icon name="envelope"/>
+        </a>
+        <a className="footer-icon-container"
+        href="https://drive.google.com/file/d/11Gc1slpp4M7KTM0HewPIKSO9ZAohG4Mo/view?usp=sharing"
+        target="_blank"
+        rel="noopener noreferrer">
+          <Icon name="file"/>
+        </a>
+      </div>
+      <div className="footer-name">
+        <p>Shijir-Erdene Bat-Enkh © 2021</p>
+      </div>
     </div>
   );
 };
